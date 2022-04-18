6ef6f107fd1ed1e3424a7155265e5af3b0a3067a
Andrea Tentner <tentner-andrea@norc.org>
2022-04-18 15:54:41 -0500

    reformatted instance_latest_to_instance_shared script for command line use

--- scripts/python/instance_latest_to_instance_shared.py
+++ scripts/python/instance_latest_to_instance_shared.py
@@ -4,45 +4,67 @@ import os, sys
 import pandas as pd
 from cedar.utils import getter, updater, searcher, storer
 
-server_address = "https://resource." + os.environ['CEDAR_HOST']
-api_key = "apiKey " + os.environ['CEDAR_API_KEY']
+def main(permission_type = "write", instance_creator_uuid = None):
+    server_address = "https://resource." + os.environ['CEDAR_HOST']
+    api_key = "apiKey " + os.environ['CEDAR_API_KEY']
 
-permission_type = "write"
-user_id = # input just the uuid here 
-template_id = "https://repo.metadatacenter.org/templates/a5c99619-7235-4a76-9c48-b51e91488a2f"
-instance_path = r"C:\Users\tentner-andrea\project_repositories\cedar-util-test-template.json"
-platform_id = None # input just the uuid here
+    template_id = sys.argv[1]
+    user_uuid = sys.argv[2]
+    permission_type = sys.argv[3]
+    instance_creator_uuid = sys.argv[4]
 
-# construct the full user ids here
-if user_id is not None:
-    user_full_id = "https://metadatacenter.org/users/" + user_id
+    # construct the full user and instance creator ids here
+    user_id = user_id_from_uuid(user_uuid)
+    
+    if instance_creator_uuid is not None:
+        instance_creator_id = user_id_from_uuid(instance_creator_uuid)
+    else:
+        instance_creator_id = None
 
-if platform_id is not None:
-    platform_full_id = "https://metadatacenter.org/users/" + platform_id
+    # get the instance of the template that was most recently created
+    # if instance creator is provided, get the instance of the template that was most recently created by the instance creator
+    instance_id = get_latest_instance(server_address, api_key, template_id, instance_creator_id)
 
-instance_ids = searcher.search_instances_of(server_address, api_key, template_id)
+    # give the user specified permissions to instance (default permission is write)
+    add_single_user_permission_to_instance(server_address, api_key, instance_id, user_id, permission_type)
 
-createdOn = []
-createdBy = []
 
-for instance_id in instance_ids:
-    # get "pav:createdOn" and "pav:createdBy" fields for each instance;
-    # add to a df with row for each instance; columns for instance id, createdOn, createdBy
-    instance_details = getter.get_instance_details(server_address, api_key, instance_id)
-    createdOn.append(instance_details['createdOnTS'])
-    createdBy.append(instance_details['pav:createdBy'])
+def user_id_from_uuid(uuid):
+    return "https://metadatacenter.org/users/" + uuid
+    
+def get_latest_instance(server_address, api_key, template_id, instance_creator_id = None):
+
+    instance_ids = searcher.search_instances_of(server_address, api_key, template_id)
+
+    createdOn = []
+    createdBy = []
+
+    for instance_id in instance_ids:
+        # get "pav:createdOn" and "pav:createdBy" fields for each instance;
+        # add to a df with row for each instance; columns for instance id, createdOn, createdBy
+        instance_details = getter.get_instance_details(server_address, api_key, instance_id)
+        createdOn.append(instance_details['createdOnTS'])
+        createdBy.append(instance_details['pav:createdBy'])
                                  
-instance_id = None #reset instance_id index used here since instance_id var name will be used later
-instances_df = pd.DataFrame({'id':instance_ids,'created_on':createdOn,'created_by':createdBy})
+    instance_id = None #reset instance_id index used here since instance_id var name will be used later
+    instances_df = pd.DataFrame({'id':instance_ids,'created_on':createdOn,'created_by':createdBy})
 
-# if provided a platform cedar user id filter the template instances by creator == platform
-if platform_id is not None:
-    platform_created = instances_df['created_by'] == platform_full_id
-    instances_df = instances_df[platform_created]
+    if len(instances_df.index) < 1:
+            raise Exception("There are no instances of the specified template")
 
-# sort by time created with latest value first and get the first row (most recently created)    
-instances_df = instances_df.sort_values(by=['created_on'], ascending=False)
-instance_id = instances_df['id'][0]
+    # if provided a instance creator id, filter the template instances by creator == platform
+    if instance_creator_id is not None:
+        creator_created = instances_df['created_by'] == instance_creator_id
+        instances_df = instances_df[creator_created]
+
+        if len(instances_df.index) < 1:
+            raise Exception("The specified user has not created any instances of the specified template")
+
+    # sort by time created with latest value first and get the first row (most recently created)    
+    instances_df = instances_df.sort_values(by=['created_on'], ascending=False)
+    instance_id = instances_df['id'][0]
+
+    return instance_id
 
 def add_single_user_permission_to_instance(server_address, api_key, instance_id, user_id, permission_type):
 
@@ -71,4 +93,6 @@ def create_user_permission(user_id, permission_type):
         'permission': permission_type
     }
 
-add_single_user_permission_to_instance(server_address, api_key, instance_id, user_full_id, permission_type)
+
+if __name__ == "__main__":
+    main()
